8bec63b74316110846c2be366ade5b091798695c
NaufalArdian12 <naufalopo87@gmail.com>
2025-04-10 16:45:41 +0700

    fix json index

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,6 +6,7 @@
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
             </div>
         </div>
         <div class="card-body">
@@ -48,60 +49,66 @@
                     </table>
                 </div>
             </div>
-        @endsection
+        </div>
+    </div>
 
-        @push('css')
-        @endpush
+    <!-- Modal -->
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"
+        aria-hidden="true"></div>
+@endsection
 
-        @push('js')
-            <script>
-                $(document).ready(function() {
-                    var dataUser = $('#table_user').DataTable({
-                        serverSide: true,
-                        ajax: {
-                            url: "{{ url('user/list') }}",
-                            dataType: "json",
-                            type: "POST",
-                            data: function(d) {
-                                d.level_id = $('#level_id').val();
-                            }
-                        },
-                        columns: [{
-                                data: "DT_RowIndex",
-                                className: "text-center",
-                                orderable: false,
-                                searchable: false
-                            },
-                            {
-                                data: "username",
-                                className: "",
-                                orderable: true,
-                                searchable: true
-                            },
-                            {
-                                data: "nama",
-                                className: "",
-                                orderable: true,
-                                searchable: true
-                            },
-                            {
-                                data: "level.level_nama",
-                                className: "",
-                                orderable: false,
-                                searchable: false
-                            },
-                            {
-                                data: "aksi",
-                                className: "",
-                                orderable: false,
-                                searchable: false
-                            }
-                        ]
-                    });
+@push('css')
+@endpush
 
-                    $('#level_id').change(function() {
-                        dataUser.ajax.reload();
-                    });
-                });
-            </script>
-        @endpush
+@push('js')
+    <script>
+        function modalAction(url = '') {
+            $('#myModal').load(url, function () {
+                $('#myModal').modal('show');
+            });
+        }
+        var dataUser;
+        $(document).ready(function () {
+            dataUser = $('#table_user').DataTable({
+                serverSide: true,
+                ajax: {
+                    url: "{{ url('user/list') }}",
+                    type: "POST",
+                    data: function (d) {
+                        d.level_id = $('#level_id').val();
+                    }
+                },
+                columns: [
+                    {
+                        data: "DT_RowIndex",
+                        name: "DT_RowIndex",
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: "username",
+                        name: "username"
+                    },
+                    {
+                        data: "nama",
+                        name: "nama"
+                    },
+                    {
+                        data: "level.level_nama",
+                        name: "level.level_nama"
+                    },
+                    {
+                        data: "aksi",
+                        name: "aksi",
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
+            });
+
+            $('#level_id').change(function () {
+                dataUser.ajax.reload();
+            });
+        });
+    </script>
+@endpush
